105ecaad2659d1213e93429a2dff7567a6f8ca16
chenhu <chenhu@borurio.com>
2026-03-23 13:55:54 +0800

    update

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -3,7 +3,7 @@ import { DocsPage, DocsBody, DocsDescription, DocsTitle } from 'fumadocs-ui/page
 import { notFound } from 'next/navigation';
 import { createRelativeLink } from 'fumadocs-ui/mdx';
 import { getMDXComponents } from '@/mdx-components';
-import Script from 'next/script';
+
 
 export default async function Page(props: { params: Promise<{ slug?: string[] }> }) {
   const params = await props.params;
@@ -31,25 +31,9 @@ export default async function Page(props: { params: Promise<{ slug?: string[] }>
               a: createRelativeLink(source, page),
             })}
           />
+          
         </DocsBody>
       </DocsPage>
-      <Script
-        id='giscus'
-        src='https://giscus.app/client.js'
-        data-repo='QingYuanO/embedded-learn-doc'
-        data-repo-id='R_kgDOO6T_Iw'
-        data-category='Announcements'
-        data-category-id='DIC_kwDOO6T_I84C5DxG'
-        data-mapping='url'
-        data-strict='0'
-        data-reactions-enabled='1'
-        data-emit-metadata='0'
-        data-input-position='bottom'
-        data-theme='preferred_color_scheme'
-        data-lang='zh-CN'
-        crossOrigin='anonymous'
-        async
-      ></Script>
     </>
   );
 }
